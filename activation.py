9c28dc1ef5550f36ceb435aa21f929ee3596b05f
tayo4pf <tyoluranti@gmail.com>
2023-07-08 17:16:44 +0100

    Rudimentarily implemented layers and activation functions, and tested

--- activation.py
+++ activation.py
@@ -6,21 +6,40 @@ class Activation:
         pass
 
 class Relu(Activation):
-    def __init__(self):
-        pass
-
     def sigma(z: np.ndarray):
         """
         Returns the input with the relu function applied to it
         Only for 1 subject - not for batches
         """
+        assert z.shape[1] == 1
         f = lambda x: 0 if x < 0 else x
-        return np.fromiter((f(xi) for xi in z), z.dtype, count = len(z))
+        a = np.fromiter((f(xi) for xi in z), z.dtype, count = len(z))
+        a = np.reshape(a, z.shape)
+        return a
     
     def partial_sigma(z: np.ndarray, a: np.ndarray):
         """
-        Returns the differential of relu function with respect to the  
+        Returns the differential of relu function with respect to the input z
+        """
+        assert z.shape[1] == 1
+        f = lambda x: 0 if x <= 0 else 1
+        return np.diagflat(np.fromiter((f(xi) for xi in z), z.dtype, count = len(z)))
+
+
+class Softmax(Activation):
+    def sigma(z: np.ndarray):
+        """
+        Returns the input with the softmax function applied to it
+        Only for 1 subject - not for batches
+        """
+        return np.exp(z) / np.sum(np.exp(z), axis = 0)
+
+
+    def partial_sigma(z: np.ndarray, a: np.ndarray):
+        """
+        Returns the differential of relu function with respect to the input z
+        Only for 1 subject, not batch
         """
         pre_hessian = -np.matmul(a, a.T)
-        diagonal = np.diag(a)
+        diagonal = np.diagflat(a)
         return np.add(diagonal, pre_hessian)
--- layer.py
+++ layer.py
@@ -1,5 +1,5 @@
 import numpy as np
-import activation.py
+import activation
 
 class Layer:
     def __init__(self, inputs: int, outputs: int, activation: Activation):
@@ -14,12 +14,30 @@ class Layer:
 
     def __forward(self, input):
         """
-        forward input through the layer using weights and biases
+        forward non-batch input through the layer using weights and biases
         """
+        assert input.shape[1] == 1
         self.input = input
         self.z = (np.matmul(self.weights, input), self.biases)
         self.a = self.activation.sigma(self.z)
         return self.a
     
-    def __backward(self, input):
+    def __backward(self, partials, learning_rate):
+        """
+        performs backpropogation to modify the weights and biases using the input partials
+        partials is a vector of the derivative of the error with respect to the output values
+        """
+        #Generate da_dz matrix
+        da_dz = self.activation.partial_sigma(self.z, self.a)
+        #Generate dz_dw matrix - several rows of the input
+        dz_dw = np.tile(self.input, (self.outputs, 1))
+        #Generate de_dz vector
+        de_dz = np.matmul(da_dz, partials)
+        self.de_dw = de_dz * dz_dw
         
+        #Generate de_dx partial vector for previous layer
+        de_dx = np.matmul(self.weights.T, de_dz)
+
+        #Update weights
+        self.weights = self.weights - (learning_rate * self.de_dw)
+        return de_dx
