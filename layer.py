493dab5df44f149726a1f2c6fbc1f81f3b2e91c6
tayo4pf <tyoluranti@gmail.com>
2023-07-10 17:29:18 +0100

    Added more atomized testing

--- layer.py
+++ layer.py
@@ -2,15 +2,16 @@ import numpy as np
 import activation
 
 class Layer:
-    def __init__(self, inputs: int, outputs: int, activation=activation.Identity()):
+    def __init__(self, inputs: int, outputs: int, activation=activation.Identity):
         """
         Constructs layer object
         Generates weights and biases from input and output sizes
         @param inputs: number of input features for layer
         @
         """
-        self.weights = np.random.rand(outputs, inputs)
-        self.biases = np.random.rand(outputs, 1)
+        self.weights = np.random.rand(outputs, inputs).astype(np.float64)
+        self.shape = (outputs, inputs)
+        self.biases = np.random.rand(outputs, 1).astype(np.float64)
         self.activation = activation
 
     def forward(self, input):
@@ -18,11 +19,12 @@ class Layer:
         forward non-batch input through the layer using weights and biases
         input is 2d array of shape (features, 1)
         """
+        assert input.shape == (self.shape[1], 1)
         self.input = input
         self.z = np.add(np.matmul(self.weights, input), self.biases)
-        assert self.z.shape[0] == len(self.weights)
-        assert self.z.shape[1] == 1
+        assert self.z.shape == (self.shape[0], 1)
         self.a = self.activation.sigma(self.z)
+        assert self.a.shape == self.z.shape
         return self.a
     
     def backward(self, partials, learning_rate):
@@ -30,18 +32,26 @@ class Layer:
         performs backpropogation to modify the weights and biases using the input partials
         partials is a vector of the derivative of the error with respect to the output values
         """
+        assert partials.shape == (self.shape[0], 1)
+
         #Generate da_dz matrix
         da_dz = self.activation.partial_sigma(self.z, self.a)
+        assert da_dz.shape == (self.shape[0], self.shape[0])
+
         #Generate dz_dw matrix - several rows of the input
         dz_dw = np.tile(self.input.T, (len(self.weights), 1))
+        assert dz_dw.shape == self.weights.shape
+
         #Generate de_dz vector
-        assert da_dz.shape[0] == partials.shape[0]
         de_dz = np.matmul(da_dz, partials)
+        assert de_dz.shape == (partials.shape)
+
         self.de_dw = de_dz * dz_dw
         self.de_db = de_dz
         
         #Generate de_dx partial vector for previous layer
-        de_dx = np.matmul(self.weights.T, de_dz)
+        dz_dx = self.weights.T
+        de_dx = np.matmul(dz_dx, de_dz)
 
         #Update weights and biases
         self.weights = self.weights - (learning_rate * self.de_dw)
--- loss.py
+++ loss.py
@@ -15,12 +15,14 @@ class MSE(Loss):
         """
         Returns the loss value of the output and target using mean squared error
         """
+        assert output.shape == target.shape, (output, target)
         return np.sum(np.square(output - target))
 
     def partial_loss(output, target):
         """
         Returns the partial differential of the loss with respect to the output
         """
+        assert output.shape == target.shape
         return -2 * (target - output)
     
 class Logistic(Loss):
@@ -28,10 +30,12 @@ class Logistic(Loss):
         """
         Returns the loss value of the output and target using logistic error
         """
+        assert output.shape == target.shape
         return -np.sum(target * np.log(output))
     
     def partial_loss(output, target):
         """
         Returns the partial differential of the loss with respect to the output
         """
+        assert output.shape == target.shape
         return -(target/output)
