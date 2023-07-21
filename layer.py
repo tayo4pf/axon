3513bec16efcc8a29996264ed5bbd69c6aee5a7b
oeo26 <oeo26@cam.ac.uk>
2023-07-21 16:01:56 +0100

    Added different optimizers - tested in optimizer_testing.ipynb Refactored some features to support this ARM64 version of requirements.txt

--- layer.py
+++ layer.py
@@ -1,5 +1,6 @@
 import numpy as np
 import activation
+from optimizer import Optimizer
 
 class Layer:
     def __init__(self, inputs: int, outputs: int, activation=activation.Identity):
@@ -15,23 +16,35 @@ class Layer:
         self.biases = np.random.rand(outputs, 1).astype(np.float64)
         self.activation = activation
 
-    def forward(self, input):
+        #Momentum attributes
+        self.weights_velocity = np.zeros(self.weights.shape).astype(np.float64)
+        self.biases_velocity = np.zeros(self.biases.shape).astype(np.float64)
+
+        #Adaptive learning rate attribute
+        self.ada_weights = np.zeros(self.weights.shape).astype(np.float64)
+        self.ada_biases = np.zeros(self.biases.shape).astype(np.float64)
+
+    def freeze(self):
         """
-        forward non-batch input through the layer using weights and biases
-        input is 2d array of shape (features, 1)
+        Sets velocity (momentum) of derivatives to zero 
+        Use to kill momentum between training sets
         """
-        assert input.shape == (self.shape[1], 1)
-        self.input = input
-        self.z = np.add(np.matmul(self.weights, input), self.biases)
-        assert self.z.shape == (self.shape[0], 1)
-        self.a = self.activation.sigma(self.z)
-        assert self.a.shape == self.z.shape
-        return self.a
-    
-    def backward(self, partials, learning_rate):
+        self.weights_velocity = np.zeros(self.weights.shape)
+        self.biases_velocity = np.zeros(self.biases.shape)
+
+    def reset_ada(self):
         """
-        performs backpropogation to modify the weights and biases using the input partials
-        partials is a vector of the derivative of the error with respect to the output values
+        Sets adaptive learning rate components to zero
+        Use to reset adaptive learning rate between training sets
+        """
+        self.ada_weights = np.zeros(self.weights.shape)
+        self.ada_biases = np.zeros(self.biases.shape)
+
+    def derivatives(self, partials):
+        """
+        Given derivative of error with respect to layer outputs,
+        computes derivative with respect to weights and biases
+        returns derivative with respect to inputs
         """
         assert partials.shape == (self.shape[0], 1)
 
@@ -53,8 +66,193 @@ class Layer:
         #Generate de_dx partial vector for previous layer
         dz_dx = self.weights.T
         de_dx = np.matmul(dz_dx, de_dz)
+        
+        return de_dx
+
+    def forward(self, optimizer, input, momentum):
+        """
+        Forwards input through network using method corresponding with optimizer
+        @param optimizer: Optimizer to be used for forward
+        @param input: Input data
+        @param momentum: Momentum float between 0 and 1 to be used if optimizer requires momentum value
+        """
+        match optimizer:
+            case Optimizer.NAG:
+                return self.forward_nag(input, momentum)
+        return self.forward_(input)
+
+    def backward(self, optimizer: Optimizer, partials: np.ndarray, learning_rate: float, momentum: float, epsilon: float, gamma: float):
+        """
+        Performs backpropogation on layer with optimizer to modify weights and biases
+        @param optimizer: Optimizer to be used for backpropagation
+        @param partials: Derivative of error with respect to output values
+        @param learning_rate: Learning rate value
+        @param momentum: Momentum preservation used in momentum based optimizers - float between 0 and 1
+        @param epsilon: Small number (default 0.01) used for adaptive learning rate
+        @param gamma: Exponential averaging variable used - float between 0 and 1
+        """
+        match optimizer:
+            case Optimizer.SGD:
+                return self.backward_sgd(partials, learning_rate)
+            case Optimizer.SGD_WM:
+                return self.backward_sgd_wm(partials, learning_rate, momentum)
+            case Optimizer.NAG:
+                return self.backward_nag(partials, learning_rate, momentum)
+            case Optimizer.AdaGrad:
+                return self.backward_adagrad(partials, learning_rate, epsilon)
+            case Optimizer.AdaDelta:
+                return self.backward_adadelta(partials, learning_rate, gamma, epsilon)
+            case Optimizer.AdaM:
+                return self.backward_adam(partials, learning_rate, momentum, gamma, epsilon)
+
+    def forward_(self, input):
+        """
+        forward non-batch input through the layer using weights and biases
+        input is 2d array of shape (features, 1)
+        """
+        assert input.shape == (self.shape[1], 1)
+
+        self.input = input
+        self.z = np.add(np.matmul(self.weights, input), self.biases)
+        assert self.z.shape == (self.shape[0], 1)
+
+        self.a = self.activation.sigma(self.z)
+        assert self.a.shape == self.z.shape
+
+        return self.a
+    
+    def backward_sgd(self, partials, learning_rate):
+        """
+        Performs backpropogation with stochastic gradient descent to modify the weights and biases using the input partials
+        @param partials: is a vector of the derivative of the error with respect to the output values
+        """
+        de_dx = self.derivatives(partials)
 
         #Update weights and biases
         self.weights = self.weights - (learning_rate * self.de_dw)
         self.biases = self.biases - (learning_rate * self.de_db)
+        return de_dx
+    
+    def backward_sgd_wm(self, partials, learning_rate, momentum):
+        """
+        Performs backpropagation with stochastic gradient descent with momentum
+        @param partials: vector of the derivative of the error with respect to the output values
+        @param momentum: float value between 0 and 1 for the proportion of derivative conserved 
+        """
+        de_dx = self.derivatives(partials)
+
+        #Update velocity
+        self.weights_velocity = (momentum * self.weights_velocity) + (learning_rate * self.de_dw)
+        self.biases_velocity = (momentum * self.biases_velocity) + (learning_rate * self.de_db)
+
+        #Update weights and biases
+        self.weights = self.weights - self.weights_velocity
+        self.biases = self.biases - self.biases_velocity
+        return de_dx
+    
+    def forward_nag(self, input, momentum):
+        """
+        forward non-batch input through the layer using the NAG future features for training
+        input is 2d array of shape (features, 1)
+        """
+        assert input.shape == (self.shape[1], 1)
+
+        future_weights = self.weights - (momentum * self.weights_velocity)
+        future_biases = self.biases - (momentum * self.biases_velocity)
+
+        self.input = input
+        self.z = np.add(np.matmul(future_weights, input), future_biases)
+        assert self.z.shape == (self.shape[0], 1)
+
+        self.a = self.activation.sigma(self.z)
+        assert self.a.shape == self.z.shape
+
+        return self.a
+    
+    def backward_nag(self, partials, learning_rate, momentum):
+        """
+        Performs backpropagation with nesterov accelerated gradient descent
+        @param partials: vector of the derivative of the error with respect to the output values
+        @param momentum: float value between 0 and 1 for the proportion of derivative conserved 
+        """
+        assert partials.shape == (self.shape[0], 1)
+
+        #Compute accelerated weights
+        future_weights = self.weights - (momentum * self.weights_velocity)
+
+        #Generate da_dz matrix
+        da_dz = self.activation.partial_sigma(self.z, self.a)
+        assert da_dz.shape == (self.shape[0], self.shape[0])
+
+        #Generate dz_dw matrix - several rows of the input
+        dz_dw = np.tile(self.input.T, (len(self.weights), 1))
+        assert dz_dw.shape == self.weights.shape
+
+        #Generate de_dz vector
+        de_dz = np.matmul(da_dz, partials)
+        assert de_dz.shape == (partials.shape)
+
+        self.de_dw = de_dz * dz_dw
+        self.de_db = de_dz
+        
+        #Generate de_dx partial vector for previous layer with accelerated weight values
+        dz_dx = future_weights.T
+        de_dx = np.matmul(dz_dx, de_dz)
+
+        #Update velocity
+        self.weights_velocity = (momentum * self.weights_velocity) + (learning_rate * self.de_dw)
+        self.biases_velocity = (momentum * self.biases_velocity) + (learning_rate * self.de_db)
+
+        #Update weights and biases
+        self.weights = self.weights - self.weights_velocity
+        self.biases = self.biases - self.biases_velocity
+        return de_dx
+    
+    def backward_adagrad(self, partials, learning_rate, epsilon):
+        """
+        Performs backpropogation with adaptive gradient descent to modify the weights and biases using the input partials
+        @param partials: is a vector of the derivative of the error with respect to the output values
+        """
+        de_dx = self.derivatives(partials)
+
+        #Update weights and biases
+        self.weights = self.weights - (learning_rate * self.de_dw/(np.sqrt(self.ada_weights + epsilon)))
+        self.biases = self.biases - (learning_rate * self.de_db/(np.sqrt(self.ada_biases + epsilon)))
+        self.ada_weights = self.ada_weights + np.square(self.de_dw)
+        self.ada_biases = self.ada_biases + np.square(self.de_db)
+        return de_dx
+    
+    def backward_adadelta(self, partials, learning_rate, gamma, epsilon):
+        """
+        Performs backpropagation with adaptive delta to modify the weights and biases using the input partials
+        @param partials: is a vector of the derivative of the error with respect to the output values
+        @param gamma: float between 0 and 1
+        """
+        de_dx = self.derivatives(partials)
+
+        #Update weights and biases
+        self.weights = self.weights - (learning_rate * self.de_dw/(np.sqrt(self.ada_weights + epsilon)))
+        self.biases = self.biases - (learning_rate * self.de_db/(np.sqrt(self.ada_biases + epsilon)))
+        self.ada_weights = (gamma * self.ada_weights) + ((1 - gamma) * np.square(self.de_dw))
+        self.ada_biases = (gamma * self.ada_biases) + ((1 - gamma) * np.square(self.de_db))
+
+        return de_dx
+    
+    def backward_adam(self, partials, learning_rate, momentum, gamma, epsilon):
+        """
+        Performs backpropagation with adam optimization to modify the weights and biases using the input partials
+        @param partials: vector of the derivative of the error with respect to the output values
+        @param momentum: momentum exponential decay variable
+        @param gamma: adaptive learning rate exponential decay variable
+        """
+        de_dx = self.derivatives(partials)
+
+        #Update weights and biases
+        self.weights_velocity = (momentum * self.weights_velocity) + ((1 - momentum) * self.de_dw)
+        self.biases_velocity = (momentum * self.biases_velocity) + ((1 - momentum) * self.de_db)
+        self.weights = self.weights - (learning_rate * self.weights_velocity/(np.sqrt(self.ada_weights + epsilon)))
+        self.biases = self.biases - (learning_rate * self.biases_velocity/(np.sqrt(self.ada_biases + epsilon)))
+        self.ada_weights = (gamma * self.ada_weights) + ((1 - gamma) * np.square(self.de_dw))
+        self.ada_biases = (gamma * self.ada_biases) + ((1 - gamma) * np.square(self.de_db))
+        
         return de_dx
--- network.py
+++ network.py
@@ -2,27 +2,52 @@ import numpy as np
 import loss
 import layer
 import activation
+from optimizer import Optimizer
 import matplotlib.pyplot as plt
 import csv
 
 class Network:
-    def __init__(self, shape: list[int], activations: list[activation.Activation], loss: loss.Loss, learning_rate: float):
+    def __init__(self, shape: list[int], activations: list[activation.Activation], loss: loss.Loss):
+        """
+        Constructs a fresh neural network object
+        @param shape: list[int] of number of inputs and outputs at each layer
+        @param activations: list[activation.Activation] of activation modules for each layer of neurons
+        @param loss: loss.Loss, loss module for the loss function of the network
+        """
         assert len(shape) - 1 == len(activations)
         self.layers = [layer.Layer(shape[i], shape[i+1], activations[i]) for i in range(len(shape)-1)]
         self.shape = shape
         self.activations = activations
-        self.learning_rate = learning_rate
         self.loss = loss
 
-    def train(self, data: np.ndarray, labels: np.ndarray):
+    def train(self, data: np.ndarray, labels: np.ndarray, optimizer: Optimizer, learning_rate: float, momentum = 0.9, epsilon = 0.01, gamma = 0.8, fresh = True):
         """
         Trains the network using the given data and labels
         @param data: 2-d numpy matrix of input subjects, each subject occupies a row
         @param labels: 2-d numpy matrix of expected network output values, each label occupies a row
         labels rows should be the same width as the final layer output
+        @param optimizer: Optimizer to be used for training
+        @param learning_rate: Learning rate - float value
+        @param momentum: Momentum value to be used for momentum based optimizers - float value between 0 and 1, default 0.9
+        @param epsilon: Epsilon value for adaptive learning rate optimizers, small float value, default 0.01
+        @param gamma: Exponential averaging value to be used for adaptive learning rate optimizers, float value between 0 and 1
+        @param fresh: Boolean variable, when set to true all momentum and adaptive learning variables will be reset to zero
         @output: numpy array of loss values for each subject
         """
         losses = []
+
+        assert momentum < 1
+        assert momentum > 0
+        assert gamma < 1
+        assert gamma > 0
+
+        if fresh:
+            for layer in self.layers:
+                layer.freeze()
+                layer.reset_ada()
+
+        if not isinstance(optimizer, Optimizer):
+            raise TypeError("Optimizer must be an instance of Optimizer Enum")
 
         #Iterate through the training data
         for subject, label in zip(data, labels):
@@ -30,7 +55,7 @@ class Network:
             label = label[np.newaxis].T
             #Forward propagate the input
             for layer in self.layers:
-                prop = layer.forward(prop)
+                prop = layer.forward(optimizer, prop, momentum)
 
             #Calculate loss
             l = self.loss.loss(prop, label)
@@ -41,7 +66,7 @@ class Network:
 
             #Backpropogate differentials through network to update weights
             for layer in reversed(self.layers):
-                de_da = layer.backward(de_da, self.learning_rate)
+                de_da = layer.backward(optimizer, de_da, learning_rate, momentum, epsilon, gamma)
 
         return np.array(losses)
 
@@ -80,7 +105,7 @@ class Network:
         for input in data:
             prop = input[np.newaxis].T
             for layer in self.layers:
-                prop = layer.forward(prop)
+                prop = layer.forward(Optimizer.SGD, prop, None)
             preds.append(prop)
         
         return np.array(preds)
@@ -108,7 +133,7 @@ class Network:
         plt.show()
 
     def write_to(self, filename):
-        fieldnames = ['inputs', 'outputs', 'weights', 'biases', 'activation', 'learning_rate', 'loss']
+        fieldnames = ['inputs', 'outputs', 'weights', 'biases', 'weights_velocity', 'biases_velocity', 'ada_weights', 'ada_biases', 'activation', 'loss']
         rows = []
 
         for layer in self.layers:
@@ -117,8 +142,11 @@ class Network:
             row['outputs'] = layer.shape[0]
             row['weights'] = layer.weights.flatten()
             row['biases'] = layer.biases.flatten()
+            row['weights_velocity'] = layer.weights_velocity.flatten()
+            row['biases_velocity'] = layer.biases_velocity.flatten()
+            row['ada_weights'] = layer.ada_weights.flatten()
+            row['ada_biases'] = layer.ada_biases.flatten()
             row['activation'] = layer.activation.name()
-            row['learning_rate'] = self.learning_rate
             row['loss'] = self.loss.name()
             rows.append(row)
 
@@ -127,7 +155,7 @@ class Network:
             writer.writeheader()
             writer.writerows(rows)
 
-    def read_from(self, filename):        
+    def __read_from(self, filename):        
         self.shape = []
         self.layers = []
         self.activations = []
@@ -140,11 +168,14 @@ class Network:
 
                 inputs = int(row['inputs'])
                 outputs = int(row['outputs'])
-                a = activation.from_name(row['activation'])
-                loz = loss.from_name(row['loss'])
-                learning_rate = float(row['learning_rate'])
+                a = activation.Activation.from_name(row['activation'])
+                loz = loss.Loss.from_name(row['loss'])
                 weights = np.array(row['weights'][1:-1].replace('\n','').split(), dtype = np.float64)
                 biases = np.array(row['biases'][1:-1].replace('\n','').split(), dtype = np.float64)
+                weights_velocity = np.array(row['weights_velocity'][1:-1].replace('\n','').split(), dtype = np.float64)
+                biases_velocity = np.array(row['biases_velocity'][1:-1].replace('\n','').split(), dtype = np.float64)
+                ada_weights = np.array(row['ada_weights'][1:-1].replace('\n','').split(), dtype = np.float64)
+                ada_biases = np.array(row['ada_biases'][1:-1].replace('\n','').split(), dtype = np.float64)
 
                 if i == 0:
                     self.shape.append(inputs)
@@ -152,11 +183,19 @@ class Network:
                 self.shape.append(outputs)
                 self.activations.append(a)
                 self.loss = loz
-                self.learning_rate = learning_rate
 
                 l = layer.Layer(inputs, outputs, a)
                 l.weights = np.reshape(weights, (outputs, inputs))
                 l.biases = np.reshape(biases, (outputs, 1))
+                l.weights_velocity = np.reshape(weights_velocity, (outputs, inputs))
+                l.biases_velocity = np.reshape(biases_velocity, (outputs, 1))
+                l.ada_weights = np.reshape(ada_weights, (outputs, inputs))
+                l.ada_biases = np.reshape(ada_biases, (outputs, 1))
                 l.shape = (outputs, inputs)
 
                 self.layers.append(l)
+
+    def read(filename):
+        nn = Network([None], [], None)
+        nn.__read_from(filename)
+        return nn
